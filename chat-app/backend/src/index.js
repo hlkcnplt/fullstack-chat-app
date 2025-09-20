554ed3a6ec7127ed0a73df03cd2ca564a6d2a81d
Haluk Canpolat <halukcnplt@gmail.com>
2025-09-20 22:19:23 +0300

    feat(backend): db connection

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -10,4 +11,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
   console.log("server is running on port " + PORT);
+  connectDB();
 });
