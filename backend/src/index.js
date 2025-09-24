50e154be27493b94de7f37977532bc27d75a6c30
Haluk Canpolat <halukcnplt@gmail.com>
2025-09-24 16:59:43 +0300

    feat(backend):  profile picture add

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -8,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
   connectDB();
 });
