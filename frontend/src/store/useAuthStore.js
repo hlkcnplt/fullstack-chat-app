e5c85c7b9eb71e69fa8d6e7287689188c61527f9
Haluk Canpolat <halukcnplt@gmail.com>
2025-09-26 23:46:02 +0300

    feat(frontend): homepage - sidebar done

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -9,8 +9,8 @@ export const useAuthStore = create((set) => ({
   isSigningUp: false,
   isLoginingIng: false,
   isUpdatingProfile: false,
-
   isCheckingAuth: true,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
